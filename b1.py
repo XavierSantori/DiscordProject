fff99dfe2af64f8d669468463af7d500519a8745
XavierSantori <118914055+XavierSantori@users.noreply.github.com>
2023-04-21 18:03:03 +0300

    Add files via upload
    Examples from sb

---
+++ b1.py
@@ -0,0 +1,41 @@
+import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+TOKEN = "MTA5ODk3NTE1NTUyODY2MzEzMQ.GCLRne.OFhHNBZyY4Lb7wK9G5qs76VeFq_z7v2o880mZ0" 
+
+
+class YLBotClient(discord.Client):
+    async def on_ready(self):
+        logger.info(f'{self.user} has connected to Discord!')
+        for guild in self.guilds:
+            logger.info(
+                f'{self.user} подключились к чату:\n'
+                f'{guild.name}(id: {guild.id})')
+
+    async def on_member_join(self, member):
+        await member.create_dm()
+        await member.dm_channel.send(
+            f'Привет, {member.name}!'
+        )
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if "привет" in message.content.lower():
+            await message.channel.send("И тебе привет")
+        else:
+            await message.channel.send("Спасибо за сообщение")
+
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+client = YLBotClient(intents=intents)
+client.run(TOKEN)
+
